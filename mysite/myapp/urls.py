dda8a943395b64c262b4c6f34dfb8835c87910e2
LebArt2321 <fifa.artem.lebedev2002@mail.ru>
2023-11-03 17:44:15 +0300

    upload item and redirect to home

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, indexItem, add_item
+from .views import index, indexItem, add_item, update_item
 
 app_name = "myapp"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', index),
     path('<int:my_id>/', indexItem, name="detail"),  
     path('additem/', add_item, name="add_item"), 
+    path('updateitem/<int:my_id>/', update_item, name="update_item"), 
 ]
