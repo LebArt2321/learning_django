fffc1d42f47c61b606f5abe836f217086203f15c
LebArt2321 <fifa.artem.lebedev2002@mail.ru>
2023-11-03 18:36:55 +0300

    create users form

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import index, indexItem, add_item, update_item
+from .views import index, indexItem, add_item, update_item, delete_item
 
 app_name = "myapp"
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('<int:my_id>/', indexItem, name="detail"),  
     path('additem/', add_item, name="add_item"), 
     path('updateitem/<int:my_id>/', update_item, name="update_item"), 
+    path('deleteitem/<int:my_id>/', delete_item, name="delete_item"), 
 ]
