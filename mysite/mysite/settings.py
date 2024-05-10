2ba191ceb167463fc518960490692f4c7b76d0ef
LebArt2321 <LebArt2321@gmail.com>
2024-05-10 20:14:02 +0300

    32323

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'main',
+        'USER': 'postgres',
+        'PASSWORD': '2321',
+        'HOST': 'localhost', # или IP-адрес сервера PostgreSQL
+        'PORT': '5432', # порт по умолчанию для PostgreSQL
     }
 }
+
 
 
 # Password validation
